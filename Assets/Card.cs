5addcad31b2a4c69b6a9f69fc3b8e033d899c9fc
qfeys <qfeys@skynet.be>
2017-07-29 03:45:20 +0200

    Creation of God and Card

---
+++ Assets/Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+    abstract class Card
+    {
+        public GameObject handCard;
+        public GameObject boardCard;
+
+        static GameObject AddToHand()
+        {
+            GameObject go = new GameObject("card", typeof(RectTransform));
+
+            return go;
+        }
+
+        class CardScript : MonoBehaviour
+        {
+
+        }
+
+    }
+}
