4d2f06a20d4ffd50dc3785c7bbb714dd813359c4
qfeys <qfeys@skynet.be>
2017-07-29 07:36:33 +0200

    camera movement

--- Assets/God.cs
+++ Assets/God.cs
@@ -19,15 +19,31 @@ public class God : MonoBehaviour {
 
     internal List<Card> activeCards;
 
+    Vector3 mouseposition;
+    float mouseZ;
+
     // Use this for initialization
     void Start () {
         if (theOne == null)
             theOne = this;
         new TestCard();
+
+        mouseZ = Camera.main.transform.position.y * Mathf.Atan(Camera.main.transform.rotation.eulerAngles.x * Mathf.Deg2Rad);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+
+        // Mouse movement
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 newMousePos = Input.mousePosition;
+            newMousePos.z = mouseZ;
+            Camera.main.transform.Translate(Camera.main.ScreenToWorldPoint(mouseposition) - Camera.main.ScreenToWorldPoint(newMousePos));
+            Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, -10);
+        }
+        mouseposition = Input.mousePosition;
+        mouseposition.z = mouseZ;
+    }
 }
